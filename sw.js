a5c0d0ff94c177f8a2f1463512bcfb3bbe5d2ccb
JABOYSKI <trentzbraswell@gmail.com>
2026-03-08 01:50:41 -0800

    Initial Tasknari PWA

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'tasknari-v1';
+const ASSETS = [
+  '/tasknari/',
+  '/tasknari/index.html',
+  '/tasknari/manifest.json',
+  '/tasknari/icon-192.png',
+  '/tasknari/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
